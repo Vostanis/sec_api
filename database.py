04fd4f0eb56940f9c45f56b9e2623120d83a8c30
Vostanis <kvostanis@outlook.com>
2022-12-21 23:18:39 +0000

    parsing nested nests

--- database.py
+++ database.py
@@ -22,31 +22,26 @@ tikr_x          = 'JNJ'
 # 2. retrieve index and CIk
 index_x         = tikr_df.index[tikr_df['ticker'] == tikr_x][0]
 cik_x           = tikr_df.iloc[[index_x]]['cik_str'].values[0]
-print(index_x)
-print(cik_x)
+# print(index_x)
+# print(cik_x)
 
 # 3. build dfs, of relevant company, via CIK
 cik_url         = f"https://data.sec.gov/submissions/CIK{cik_x}.json"
 concepts_url    = f"https://data.sec.gov/api/xbrl/companyconcept/CIK{cik_x}/us-gaap/AccountsPayableCurrent.json"
 facts_url       = f"https://data.sec.gov/api/xbrl/companyfacts/CIK{cik_x}.json"
-print(facts_url)
 
 # send the request with a Firefox header (keeps it generalised; would need an email otherwise)
-cik_df          = pd.json_normalize(json.loads((requests.get(cik_url,       headers={"User-Agent": "Mozilla/5.0"})).text))
-concepts_df     = pd.json_normalize(json.loads((requests.get(concepts_url,  headers={"User-Agent": "Mozilla/5.0"})).text))
-facts_df        = pd.json_normalize(json.loads((requests.get(facts_url,     headers={"User-Agent": "Mozilla/5.0"})).text))
+cik_response         = [json.loads((requests.get(cik_url,         headers={"User-Agent": "Mozilla/5.0"})).text)]
+concepts_response    = [json.loads((requests.get(concepts_url,    headers={"User-Agent": "Mozilla/5.0"})).text)]
+facts_response       = [json.loads((requests.get(facts_url,       headers={"User-Agent": "Mozilla/5.0"})).text)]
+print(concepts_response)
+cik_df          = pd.json_normalize(cik_response)
+concepts_df     = pd.json_normalize(concepts_response)
+facts_df        = pd.json_normalize(facts_response)
 
-# cik_df = pd.read_json(cik_response)
-print(cik_df.columns.tolist())
+# print(cik_df.columns.tolist())
 print(concepts_df.columns.tolist())
-print(facts_df.columns.tolist())
-# pd.reset_option('max_columns')
-print(cik_df['filings.recent.acceptanceDateTime'].values[0])
-print(cik_df['filings.recent.form'].values[0])
-# filings_df = pd.DataFrame(json.loads(cik_df['filings.files'].values[0]))
-# print(filings_df)
-# filings_df = json.loads(cik_df['filings.files'].text)
-# print(filings_df.columns.tolist())
+# print(facts_df.columns.tolist())
 
-# print(cik_df)
-# cik_df = pd.DataFrame(json_normalize(cik_response))
+revenue_df      = pd.json_normalize(concepts_response, record_path=['units'], meta=['USD.val', 'USD.filed'])
+print(revenue_df)
